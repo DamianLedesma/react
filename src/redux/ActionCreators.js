30c5d11734783d47d3b3213884cbb2159619c58f
Damian Ledesma <erik.ledesma@gmail.com>
2020-04-21 02:38:28 +0200

    Redux Thunk

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -23,12 +23,11 @@ export const dishesLoading = () => ({
     type: ActionTypes.DISHES_LOADING
 });
 
-export const dishesFailed = (errMess) => {
-    return ({
+export const dishesFailed = (errmess) => ({
         type: ActionTypes.DISHES_FAILED,
-        payload: errMess
-    });
-};
+        payload: errmess
+});
+
 
 export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
